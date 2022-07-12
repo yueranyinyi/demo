7e28b0d2ed7c790f80e8700b03c7666198e2d520
eaainwj <894832510@qq.com>
2022-07-12 19:35:59 +0800

    hotfix commit1

--- src/main/java/jianxue/wang/demotest.java
+++ src/main/java/jianxue/wang/demotest.java
@@ -9,6 +9,7 @@ public class demotest {
     public static void main(String[] args) {
         System.out.println("ok");
         System.out.println("ok-hotfix");
+        System.out.println("ok-hotfix2");
 
     }
 }
